2aeb7bde2adc5c99377539776f1a7a2e52aae8b3
Luiz Mendes <developer.luizmendes@gmail.com>
2021-06-19 12:12:06 -0300

    correção posição versionamento rota

--- src/Veiculo/controller.js
+++ src/Veiculo/controller.js
@@ -2,7 +2,7 @@ const routes = require('express').Router()
 const model = require('./model')
 const recurso = 'veiculo'
 
-routes.get(`/${recurso}/v1`,(req,res,next)=>{
+routes.get(`/v1/${recurso}`,(req,res,next)=>{
     const obj = model.teste()
     res.status(200).json(obj)
 })
