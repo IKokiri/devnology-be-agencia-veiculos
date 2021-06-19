7b5e56176f7c9155a2221636f8679ce7e112b892
Luiz Mendes <developer.luizmendes@gmail.com>
2021-06-19 15:23:07 -0300

    criando testes para marcas

--- src/Marca/controller.js
+++ src/Marca/controller.js
@@ -3,8 +3,12 @@ const model = require('./model')
 const recurso = 'marca'
 
 routes.post(`/v1/${recurso}`,(req,res,next)=>{
-    const obj = model.create(req.body)
-    res.status(201).json(obj)
+    try{
+        const obj = model.create(req.body)
+        res.status(201).json(obj)
+    }catch(error){
+        res.status(500).json({"error":"a marca nao pode ser criada, contate o suporte."})
+    }
 })
 
 module.exports = routes
