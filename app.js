68f7961df589ab756d7d0b4a89b2d1db6e01fc69
Luiz Mendes <developer.luizmendes@gmail.com>
2021-06-19 13:12:25 -0300

    alternativa de porta para heroku

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const veiculoApi = require('./src/Veiculo/controller')
 
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 4000
 /**
  * Rota principal
  */
 app.use('/agencia/',veiculoApi);
 
-app.listen(4000,()=>{
+app.listen(PORT,()=>{
     console.log("Olá luiz")
 })
