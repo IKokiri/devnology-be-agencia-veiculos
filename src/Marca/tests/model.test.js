17e76135f4ac11b9fd27be7c950072845d510b2f
Luiz Mendes <developer.luizmendes@gmail.com>
2021-06-20 08:56:23 -0300

    segregação de ambientes

--- src/Marca/tests/model.test.js
+++ src/Marca/tests/model.test.js
@@ -1,5 +1,5 @@
 const model = require('../model')
 it('deve criar uma marca',async()=>{
-  const marca = await model.create({"marca":"polo"})
+  const marca = await model.create({"marca":"polos"})
   expect(1).toBe(1)
 })
