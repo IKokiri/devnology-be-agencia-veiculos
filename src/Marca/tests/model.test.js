4e056cba34838fc9add9ee3f4998e2a5c9cf31f8
Luiz Mendes <developer.luizmendes@gmail.com>
2021-06-20 11:42:52 -0300

    teste do crud básico

--- src/Marca/tests/model.test.js
+++ src/Marca/tests/model.test.js
@@ -1,9 +1,44 @@
 const model = require('../model')
-it('deve criar uma marca',async()=>{
-  
-  const marca = await model.create({"marca":"polos"})
-  
+var tempId = 0;
+const data = {"marca":"FIAT"}
 
+it('deve criar uma marca',async()=>{  
 
-  expect(1).toBe(1)
+  const marca = await model.create(data)
+  
+  tempId = marca.id
+  result = {"marca": marca.marca}
+
+  expect(result).toEqual(data)
+
+})
+
+it('deve buscar todos os registros no banco, nesse caso 2',async()=>{
+  /**
+   * cria mais uma marca
+   */
+  dataTran = {"marca":"RENAULT"}  
+  await model.create(dataTran)
+
+  const marca = await model.findAll()
+  expect(marca.length).toEqual(2)
+
+})
+
+it('deve buscar o objeto com o id do primeiro teste criado',async()=>{
+  const marca = await model.findByPk(tempId)
+
+  result = {"marca": marca.marca}
+  expect(result).toEqual(data)
+})
+
+it('deve alterar o objeto com o primeiro id para HONDA',async()=>{
+  const dataUpdate = {"marca":"HONDA"}
+  const result = await model.update(tempId,dataUpdate)
+  expect(result[0]).toEqual(1)
+})
+
+it('deve excluir o primeiro registro de insersão inicial',async()=>{
+  const result = await model.delete(tempId)
+  expect(result).toEqual(1)
 })
