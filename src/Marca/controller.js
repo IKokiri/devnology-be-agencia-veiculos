96267f1b40561519e93a6dd49ea269939e89d22a
Luiz Mendes <developer.luizmendes@gmail.com>
2021-06-20 06:17:12 -0300

    adição de chamada sincrona

--- src/Marca/controller.js
+++ src/Marca/controller.js
@@ -2,9 +2,9 @@ const routes = require('express').Router()
 const model = require('./model')
 const recurso = 'marca'
 
-routes.post(`/v1/${recurso}`,(req,res,next)=>{
+routes.post(`/v1/${recurso}`, async (req,res,next)=>{
     try{
-        const obj = model.create(req.body)
+        const obj = await model.create(req.body)
         res.status(201).json(obj)
     }catch(error){
         res.status(500).json({"error":"a marca nao pode ser criada, contate o suporte."})
--- src/Marca/tests/model.test.js
+++ src/Marca/tests/model.test.js
@@ -1,4 +1,5 @@
+const model = require('../model')
 it('deve criar uma marca',async()=>{
-  // const marca = model.create({"marca":"polo"})
+  const marca = await model.create({"marca":"polo"})
   expect(1).toBe(1)
 })
